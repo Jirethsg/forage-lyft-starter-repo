fa9c28d755ba04d72a887cd6de82c2d8f9cdab87
Jireth Soriano | Java| Python| SQL <121898179+Jirethsg@users.noreply.github.com>
2023-06-14 18:42:50 -0400

    Last changes

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,10 +1,9 @@
 from engine.engine import Engine
 
 class CapuletEngine(Engine):
-    def __init__(self, current_mileage, last_mileage):
-        self.current__mileage = current_mileage
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def needs_be_service(self):
+    def needs_service(self):
         return self.current_mileage - self.last_service_mileage > 30000
